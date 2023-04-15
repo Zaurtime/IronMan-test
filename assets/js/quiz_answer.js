335fa2b73ac03ebe8570519b3206eaf2cccdfb93
Zaurtime <zaurteam@gmail.com>
2023-04-15 18:08:22 +0000

    Create js files for quiz and answers plus add text

---
+++ assets/js/quiz_answer.js
@@ -0,0 +1,6 @@
+let answers = {
+    "s" : {
+        "description" : "You are just like Iron Man,a brash but brilliant character.Always the life of the party and the centre of attention,you like see the fun in every occasion!Often catalyst amongst friends you are there for them no matter the occasion!"
+        
+    }
+}
