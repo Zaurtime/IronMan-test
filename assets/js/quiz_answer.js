30e30287a1c638513a6955d4a4584d0200baec46
Zaurtime <zaurteam@gmail.com>
2023-04-16 20:04:35 +0000

    Delete old quiz_answers js file

--- assets/js/quiz_answer.js
+++
@@ -1,6 +0,0 @@
-let answers = {
-    "s" : {
-        "description" : "You are just like Iron Man,a brash but brilliant character.Always the life of the party and the centre of attention,you like see the fun in every occasion!Often catalyst amongst friends you are there for them no matter the occasion!"
-        
-    }
-}
