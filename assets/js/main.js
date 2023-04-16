a04b065b77903dcd89ccdbc45ebc8c5653dfc0ab
Zaurtime <zaurteam@gmail.com>
2023-04-16 13:01:26 +0000

    Change name and new test text to quiz

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: 'What is the most difficult IronMan race?',
+        answers: [
+                  'Patagonman, Chile', 
+                  'Cork, Ireland', 
+                  'Lanzarote, Canary Islands', 
+                  'Norseman Xtreme, Norway'
+                ],
+        correct: 1,
+    },
+    {
+        question: 'How many km an IronMan run daily?',
+        answers: ['10km', '42km', '1km', 'Next Monday 5km'],
+        correct: 2,
+    },
+    {
+        question: 'How many km can you swim?',
+        answers: ['100m', '200m', '2km', '3km and 800meters'],
+        correct: 4,
+    }
